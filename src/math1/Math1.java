251e562a6eb5198e4e09acb46a98bd6f94b3b79e
yuwan5525 <yuwan5525@7HKP6Y1.Student.UGDSB.ED>
2018-03-20 13:33:52 -0400

    Nydb cj6gauC

---
+++ src/math1/Math1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package math1;
+
+/**
+ *
+ * @author yuwan5525
+ */
+public class Math1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double Meal = 8.95;
+        // the cost of the meal;
+        double Host = 1.16;
+        // the text they have got;
+        double Total = Meal + Host;
+        // total they need to pay;
+        System.out.print(Total);
+    }
+    
+}
